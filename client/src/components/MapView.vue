68d5d5a65d1a28fee66c38a67d199d58b3f04e4b
Josephus Paye II <j.paye96@gmail.com>
2016-08-17 15:37:18 +1000

    Add Indicator and MapView components

---
+++ client/src/components/MapView.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="view map-view">Map</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello world!'
+        };
+    }
+};
+</script>
+
+<style lang="stylus">
+@import '~styles/_variables';
+</style>
