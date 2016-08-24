6e1d5285192e0f4ac68592f1c903d1faf27dae29
Josephus Paye II <j.paye96@gmail.com>
2016-08-24 13:44:32 +1000

    Add config file

---
+++ client/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    googleMaps: {
+        key: 'AIzaSyABnCcekyPecGnsA1Rj_NdWjmUafJ1yVqA',
+        v: 3
+    },
+    map: {
+        center: {
+            lat: -32.8883338,
+            lng: 151.7075279
+        },
+        zoom: 19,
+        disableDefaultUI: true
+    },
+    settings: {
+        defaultMapType: 'SATELLITE'
+    }
+};
